5046ce874f4ac0524d87cb21b07782e694b874b0
camilo-camargo <camilocamargo49@gmail.com>
2023-01-12 14:48:30 -0500

    Category resolvers, Done.

--- src/resolver/channel/categories.py
+++ src/resolver/channel/categories.py
@@ -1,2 +1,15 @@
-def categories(_, info):
-    return ["[categories] Not implemented yet"]
+import pandas as pd
+from data import db_categories
+from utils import find_value
+
+
+def categories(obj, info):
+    value = obj["categories"]
+    if not pd.isnull(value):
+        values = value.split(";")
+        print(values)
+        _categories = []
+        for category in values:
+            _categories.append(find_value(db_categories, "id", category))
+        return _categories
+    return None
