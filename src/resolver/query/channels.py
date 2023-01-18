a89734c8bd56083848692cf615ff539b4466f994
camilo-camargo <camilocamargo49@gmail.com>
2023-01-18 18:45:03 -0500

    Pattern matching in channels, Done.

--- src/resolver/query/channels.py
+++ src/resolver/query/channels.py
@@ -1,9 +1,22 @@
 from db import db
 
 
-def channels(*_, length):
+def channels(*_, length=None, search=None, stream=False):
+    QUERY = "SELECT Channel.* from Channel "
+
+    if stream:
+        QUERY += "JOIN Stream using (id_channel) "
+    if search:
+        QUERY += f'WHERE Channel.name LIKE "%{search}%" '
+
+    if stream:
+        QUERY += 'AND Stream.url IS NOT NULL '
+
+    if length:
+        QUERY += f'LIMIT {length} '
+
+    print(QUERY)
     cursor = db.cursor()
-    QUERY = f"SELECT * from Channel LIMIT {length}"
     cursor.execute(QUERY)
     values = cursor.fetchall()
     cursor.close()
