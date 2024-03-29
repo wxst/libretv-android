b6d5904dc7b01482fd74ceb5e39a55bb3a86401a
camilo-camargo <camilocamargo49@gmail.com>
2024-03-29 10:51:10 -0500

    - Add cors.

--- core/src/api.py
+++ core/src/api.py
@@ -1,9 +1,14 @@
 from ariadne import make_executable_schema
 from ariadne.asgi import GraphQL
-
-from schema import types, type_defs
+from schema import types
 from resolver import resolvers
-
+from starlette.middleware.cors import CORSMiddleware
 
 schema = make_executable_schema(types, resolvers)
-app = GraphQL(schema, debug=True)
+app = CORSMiddleware(
+    GraphQL(schema, debug=True),
+    allow_origins=['*'],
+    allow_methods=("GET", "POST", "OPTIONS", ),
+    allow_headers=['access-control-allow-origin',
+                   'authorization', 'content-type']
+)
